ae0a70a67275a9c75105068256d7da1b6068a4f9
DaUn <85550617+gimdaun@users.noreply.github.com>
2022-01-20 19:25:13 +0900

    Add files via upload
    문제를 잘못이해해서 다시 코드작성해 올립니다!

--- MSA20220120/김다운_Ext1.py
+++ MSA20220120/김다운_Ext1.py
@@ -3,23 +3,38 @@ dicData1 = { "국어": 95, "영어": 90, "수학": 80, "과학": 50 }
 dicData2 = { "국어": 100, "영어": 50, "수학": 90, "과학": 90 }
 dicData3 = { "국어": 99, "영어": 60, "수학": 100, "과학": 40 }
 dicData4 = { "국어": 55, "영어": 80, "수학": 80, "과학": 60 }
-
 dic=[dicData1,dicData2,dicData3,dicData4]
 sum=0
-ave_sum=0
 all_sum=0
+all_ave=0
+print("\n개인 합계, 평균 : ")
 for i in dic:
+    #print(i)
     for j in i.values():
         sum=sum+j
-    #print(sum)
+    print(sum,end=' ') 
     ave=sum/len(i.keys())
-    #print(ave)
+    print(ave,end=' ')
+    print()
     all_sum=sum+all_sum
-    ave_sum=ave+ave_sum
+    all_ave=ave+all_ave
     sum=0
     average = 0
-all_ave=ave_sum/len(i.keys())
-print(all_sum)
-print(all_ave)
+print("\n전체 4명의 합계, 평균 : ")
+print(all_sum,end=' ')
+print(all_ave,end=' ')
+    
+print("\n과목별 합계")
+for num in range(4):
+    for i in dic:
+        value=list(i.values())
+        #print(value)
+        sum=sum+value[num]       
+    ave=sum/len(i.keys())
+    print(sum,end=' ')
+    print(ave,end=' ')
+    print()
+    sum=0
+    ave=0
 
 temp = 0
